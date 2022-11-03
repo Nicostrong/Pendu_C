3ab8ae580d06f0e0b5c66a9ecaed6704bef27d77
Nicolas FORDOXCEL <nicostrong@msn.com>
2022-11-03 16:44:06 +0100

    Fichier de base du jeu

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Pendu
+//
+//  Created by Nicolas Fordoxcel on 27/10/2022.
+//
+
+ #include <stdio.h>
+ #include "Pendu_FX.h"
+ 
+ int main(int argc, const char * argv[])
+ {
+     printf("Bienvenue au jeu du PENDU.\n");
+     MenuPendu();
+     
+     return 0;
+ }
